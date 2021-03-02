143eb07dd3d57af0c083a1d7783b07e5281a76f3
Erik Jepsen <5465915+ejep520@users.noreply.github.com>
2021-03-02 13:11:21 -0800

    Back from lunch
    Tidying up some unnecessary dependencies.

--- a404exercise_createAnAndroidLibraryRemake/ImageActivity/src/main/java/com/jepster/imageactivity/ImageFragment.java
+++ a404exercise_createAnAndroidLibraryRemake/ImageActivity/src/main/java/com/jepster/imageactivity/ImageFragment.java
@@ -9,11 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ImageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ImageFragment extends Fragment {
 
     public ImageFragment() {
